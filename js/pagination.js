0a20650eec289335173ced34cddcb173dcee36ae
$usccy <susccy@gmail.com>
2022-03-02 13:22:06 +0100

    documentation and slight refactoring

--- js/pagination.js
+++ js/pagination.js
@@ -1,17 +1,23 @@
 const PRODUCTS_PER_PAGE = 15
 
-const productContainer = document.getElementById("product-container")
-const productStore = [...productContainer.children]
+const productContainer = document.getElementById("product-container") // contains only the currently shown products
+const productStore = Object.freeze(Array.from(productContainer.children)) // always contains all initial products
 const paginationList = document.getElementById("pagination-list")
 const categoryList = document.getElementById("category-list")
 const subcategoryList = document.getElementById("subcategory-list")
 const brandList = document.getElementById("brand-list")
 
+/**
+ * calculates the amount of pages required to show all currently active products
+ * taking into account the value of PRODUCTS_PER_PAGE,
+ * then creates the respective amount of page buttons and appends them to the pagination list in the DOM
+ */
 function paginate() {
-  paginationList.innerHTML = ""
+  paginationList.innerHTML = "" // clear pagination buttons
 
-  const productsArray = Array.from(productContainer.children)
+  const productsArray = Array.from(productContainer.children) // get currently active products
 
+  // if no currently active products available, create a placeholder text and abort pagination
   if (!productsArray.length) {
     const noProductsPlaceholder = document.createElement("p")
     noProductsPlaceholder.appendChild(
@@ -21,29 +27,40 @@ function paginate() {
     return
   }
 
-  const pageAmount = Math.ceil(productsArray.length / PRODUCTS_PER_PAGE)
+  const pageAmount = Math.ceil(productsArray.length / PRODUCTS_PER_PAGE) // calculate required amount of pages
 
-  let currentPage
+  let currentPage // stores the currently active page for the next page button
 
+  /**
+   * fills the DOM product container with products for the given page number
+   * @param {number} pageNumber the page to open
+   * @returns curried function to be run by onclick event handler
+   */
   function openPage(pageNumber) {
     return function () {
-      productContainer.innerHTML = ""
       currentPage = pageNumber
+
       const productsForCurrentPage = productsArray.slice(
         PRODUCTS_PER_PAGE * (currentPage - 1),
         PRODUCTS_PER_PAGE * (currentPage - 1) + PRODUCTS_PER_PAGE
-      )
+      ) // get products for current page from the active products
+
+      productContainer.innerHTML = "" // clear product container
+
       for (const product of productsForCurrentPage)
-        productContainer.appendChild(product)
+        productContainer.appendChild(product) // add products to DOM container
+
+      // remove current-page class from previous page button and add it to current page button
       paginationList
-        .querySelectorAll(":not(:last-child):not(:nth-last-child(2))")
-        .forEach((el) => el.removeAttribute("class"))
+        .querySelector(".current-page")
+        ?.classList.remove("current-page")
       paginationList
         .querySelector(`:nth-child(${currentPage})`)
         .setAttribute("class", "current-page")
     }
   }
 
+  // create all necessary page number buttons and append to DOM
   for (let i = 1; i <= pageAmount; i++) {
     const pageButton = document.createElement("span")
     const pageNumber = document.createTextNode(i.toString())
@@ -52,6 +69,7 @@ function paginate() {
     paginationList.appendChild(pageButton)
   }
 
+  // create next and last page buttons
   const nextPageButton = document.createElement("span")
   const lastPageButton = document.createElement("span")
 
@@ -68,18 +86,22 @@ function paginate() {
   paginationList.appendChild(nextPageButton)
   paginationList.appendChild(lastPageButton)
 
-  openPage(1)()
+  openPage(1)() // open page 1 at start by default
 }
 
+// store all unique categories, subcategories and brands
 const categoryNames = new Set()
 const subcategoryNames = new Set()
 const brandNames = new Set()
+
+// variable arrays to contain the currently active filters
 const activeFilters = {
   categories: [],
   subcategories: [],
   brands: [],
 }
 
+// get all categories, subcategories and brands from the product data attributes and add them to their respective sets
 for (const {
   dataset: { categories, subcategories, brands },
 } of productStore) {
@@ -97,6 +119,18 @@ for (const {
     .map(brandNames.add, brandNames)
 }
 
+/**
+ * gets all products matching the currently active filter checkboxes from the store
+ * and adds them to the DOM container, then calls pagination
+ *
+ * filter logic:
+ * - if no checkboxes are active, all products are shown
+ * - if one or more checkboxes of a given type (categories, subcategories, brands) are active,
+ *    all products matching **any** of the filters within that type are shown
+ * - if checkboxes across different filter types are active,
+ *    only products matching at least one within **all** different types are shown
+ * @param {Event} param0 change event emitted by the filter checkbox
+ */
 function filter({ target }) {
   const [filterType, filterValue] = target.name.split(/-/)
 
@@ -110,9 +144,11 @@ function filter({ target }) {
   productContainer.innerHTML = ""
 
   if (
-    !activeFilters.categories.length &&
-    !activeFilters.subcategories.length &&
-    !activeFilters.brands.length
+    !(
+      activeFilters.categories.length +
+      activeFilters.subcategories.length +
+      activeFilters.brands.length
+    )
   )
     for (const product of productStore) productContainer.appendChild(product)
   else {
